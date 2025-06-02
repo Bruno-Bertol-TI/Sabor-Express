055e4938358c400bf9b998df34618190f4b06ffa
Bruno Bertol <bbertol2312@outlook.com>
2025-06-02 16:57:40 -0300

    feat(utils): adicionar funções utilitárias para limpar tela e exibir menus
    Inclui funções para limpar a tela do terminal, exibir título, menu principal e pausar a
    execução para melhorar a interface do usuário no console.

---
+++ utils.py
@@ -0,0 +1,21 @@
+import os
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def exibir_titulo():
+    print("""
+    === Sistema de Gestão de Restaurantes ===
+    """)
+
+def exibir_menu():
+    print("""
+    1. Cadastrar restaurante
+    2. Listar restaurantes
+    3. Ativar / Desativar restaurante
+    4. Sair
+    """)
+
+def pausar():
+    input("\nPressione ENTER para voltar ao menu...")
+    limpar_tela()
