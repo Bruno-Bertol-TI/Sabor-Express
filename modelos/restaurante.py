75208779293bf9f9ef6a11fe112d9b56e216e12c
Bruno Bertol <bbertol2312@outlook.com>
2025-06-06 16:23:18 -0300

    Feat: Criar funcao para calculo da nota de avaliacao, atuallizar exibicao de restaurantes (adicionar media na exibicao)

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -36,16 +36,24 @@ class Restaurante:
     def avaliacao(self):
         return self._avaliacao
     
+    @property
+    def media(self):
+        if not self._avaliacao:
+            return 0
+        notas = [a.nota for a in self._avaliacao]
+        self._media = round(sum(notas) / len(notas), 2)
+        return self._media
+        
     @classmethod
     def exibir_restaurantes(cls):
-        linha_divisoria_exibir_restaurantes = f'+ {1 * '---'} + {10 * '---'} + {10 * '---'} + {4 * '--'} +'
+        linha_divisoria_exibir_restaurantes = f'+ {1 * '---'} + {10 * '---'} + {10 * '---'} + {4 * '--'} + {5 * '--'} +'
         if not cls.lista_restaurantes:
             print('Nenhum restaurante cadastrado.')
             return
-        print(f"| {'ID':<3} | {'Nome':<30} | {'Categoria':<30} | {'Status':<8} |")
+        print(f"| {'ID':<3} | {'Nome':<30} | {'Categoria':<30} | {'Status':<8} | {'Nota média':^10} |")
         print(linha_divisoria_exibir_restaurantes)
         for id, r in enumerate(cls.lista_restaurantes):
-            print(f"| {id:^3} | {r.nome:<30} | {r.categoria:<30} | {r.ativo:^8} |")
+            print(f"| {id:^3} | {r.nome:<30} | {r.categoria:<30} | {r.ativo:^8} | {r.media:^10} |")
             print(linha_divisoria_exibir_restaurantes)
 
     @classmethod
@@ -58,7 +66,7 @@ class Restaurante:
                 for a in r.avaliacao:
                     print(f'| {id:^3} | {r.nome:<30} | {r.categoria:<30} | {r.ativo:^8} | {a.cliente:<20} | {a.nota:^5} |')
                     print(linha_divisoria_exibir_avaliacoes)
-
+    
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
